0d65e80f9ea226053bd8a356e48319257b7a4d6a
Shubham Sharma <21bce043@nith.ac.in>
2025-09-22 18:50:29 +0530

    ssr issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+export const dynamic = "force-static";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
