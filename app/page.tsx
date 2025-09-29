edba204178b4206bb72a7f786fdba71e42440d97
Shubham Sharma <21bce043@nith.ac.in>
2025-09-29 17:08:37 +0530

    added grid

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        {/* <Grid />
-        <RecentProjects />
+        <Grid />
+        {<RecentProjects />
         <Clients />
         <Experience />
         <Approach />
