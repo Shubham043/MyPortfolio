6c4d298b5792ff802b2179cf09b220f2b2eabf0a
Shubham Sharma <21bce043@nith.ac.in>
2025-09-29 17:09:07 +0530

    added grid1

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <Hero />
         <Grid />
-        {<RecentProjects />
+        {/* <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
